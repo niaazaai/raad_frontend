b7d6c41d5aef6fb9092de865f14e7876b6a10be9
Maseeh Niazaai <maseeh.niazaai@gmail.com>
2026-06-01 05:52:37 +0000

    Update MainLayout component to adjust main content margin for improved layout consistency

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -19,7 +19,7 @@ const MainLayout = () => {
       >
         <Header />
 
-        <main className="flex min-h-0 flex-1 flex-col p-4 pt-8 lg:pt-16">
+        <main className="mt-16 flex min-h-0 flex-1 flex-col p-4">
           <div className="flex h-full min-h-0 min-w-0 flex-1 flex-col">
             <Outlet />
           </div>
